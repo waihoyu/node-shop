d38719203b3126989f5d3e836a424e2808359958
waihoyu <13701343809@qq.com>
2019-10-17 11:10:40 +0800

    init this project

---
+++ README.md
@@ -0,0 +1,26 @@
+# node-shop
+
+> 一个koa+node网上商城
+
+> 项目的说明
+
+- 1、UI界面使用了有赞vant库。
+
+
+## Build Setup
+
+``` bash
+# install dependencies
+npm install
+
+# serve with hot reload at localhost:8080
+npm run dev
+
+# build for production with minification
+npm run build
+
+# build for production and view the bundle analyzer report
+npm run build --report
+```
+
+For a detailed explanation on how things work, check out the [guide](http://vuejs-templates.github.io/webpack/) and [docs for vue-loader](http://vuejs.github.io/vue-loader).
---
+++ src/components/pages/ShoppingMall.vue
@@ -0,0 +1,72 @@
+<template>
+  <div>
+    <div class="search-bar">
+      <van-row>
+        <van-col span="3">
+          <img :src="locationIcon" alt width="80%" class="location-icon" />
+        </van-col>
+        <van-col span="16">
+          <input type="text" class="search-input" />
+        </van-col>
+        <van-col span="5">
+          <van-button size="mini">查找</van-button>
+        </van-col>
+      </van-row>
+    </div>
+    <div class="swipe-area">
+      <van-swipe :auto="1000">
+        <van-swipe-item v-for="(banner, index) in bannerPicArray" :key="index">
+          <img v-lazy="banner.imageUrl" alt width="100%" />
+        </van-swipe-item>
+      </van-swipe>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      msg: "shoppingMall",
+      locationIcon: require("../../assets/images/location.png"),
+      bannerPicArray: [
+          {imageUrl: require('../../assets/images/01.jpg')},
+          {imageUrl: require('../../assets/images/02.jpg')},
+          {imageUrl: require('../../assets/images/03.jpg')},
+          {imageUrl: require('../../assets/images/04.jpg')}
+
+      ]
+    };
+  }
+};
+</script>
+
+<style scoped>
+.location-icon {
+  padding-top: 0.2rem;
+  padding-left: 0.3rem;
+}
+.search-bar {
+  height: 2.2rem;
+  background-color: #e5017d;
+  line-height: 2.2rem;
+  overflow: hidden;
+}
+.search-input {
+  width: 100%;
+  height: 1.3rem;
+  border-top: 0px;
+  border-left: 0px;
+  border-right: 0px;
+  border-bottom: 1px solid #fff !important;
+  /* border-radius: 5px; */
+  background-color: #e5017d;
+  color: #fff;
+}
+.swipe-area{
+    clear: both;
+    background-color: red;
+    max-height: 15rem;
+    overflow: hidden;
+}
+</style>
